d380775539ebd9d3daaf94fb4076892895b531d8
AntonVoronezh <pstut@mail.ru>
2019-04-22 09:06:52 +0300

    init

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+const Header = () => {
+    return (
+        <div className="header">
+            <img src="https://assets.turbologo.com/assets/landing/icons/3-d65c7f63ceeb6ce4b32c9bfd3e2cd88894e166d797521c5a426b2ce8d4a161a8.png" />
+        </div>
+    )
+}
