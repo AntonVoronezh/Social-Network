dece0323ecbb66316ff48f6bd01d4d599334957c
AntonVoronezh <pstut@mail.ru>
2019-04-22 20:25:38 +0300

    add react-router-dom

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,11 +5,11 @@ const Navbar = () => {
 	return (
 		<div className={classes.sidebar}>
 			<ul>
-				<li><a className={classes.item}>Profile</a></li>
-				<li><a className={`${classes.item} ${classes.active}`}>Messages</a></li>
-				<li><a className={classes.item}>News</a></li>
+				<li><a href="/profile" className={classes.item}>Profile</a></li>
+				<li><a href="/dialogs" className={`${classes.item} ${classes.active}`}>Messages</a></li>
+				{/* <li><a className={classes.item}>News</a></li>
 				<li><a className={classes.item}>Music</a></li>
-				<li><a className={classes.item}>Settings</a></li>
+				<li><a className={classes.item}>Settings</a></li> */}
 			</ul>
 		</div>
 	);
