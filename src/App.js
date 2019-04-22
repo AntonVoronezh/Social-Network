70602b239272bd112d81d4d33f76aad33fcfc570
AntonVoronezh <pstut@mail.ru>
2019-04-22 17:13:36 +0300

    add Dialogs component

--- src/App.js
+++ src/App.js
@@ -3,17 +3,21 @@ import './App.css';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
+import Dialogs from './components/Dialogs/Dialogs';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="wrapper">
-        <Header />
-        <Navbar />
-        <Profile />
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="wrapper">
+				<Header />
+				<Navbar />
+				<div className="content">
+					{/* <Profile /> */}
+					<Dialogs />
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
