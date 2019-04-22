80946309f9526a66a3a82cac027ea75b32ac7b62
AntonVoronezh <pstut@mail.ru>
2019-04-22 15:21:05 +0300

    add

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import classes from './Profile.module.css'
+import classes from './Profile.module.css';
+import MyPosts from './MyPosts/MyPosts';
 
 const Profile = () => {
 	return (
 		<div className={classes.content}>
-			<img src="https://awesomeworld.ru/wp-content/uploads/2017/11/nebo_7-560x280.jpg" alt="d" />
+			<div>
+				<img src="https://awesomeworld.ru/wp-content/uploads/2017/11/nebo_7-560x280.jpg" alt="d" />
+			</div>
+			<div>ava + description</div>
+			<MyPosts />
 		</div>
 	);
 };
