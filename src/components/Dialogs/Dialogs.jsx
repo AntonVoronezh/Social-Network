3d9c20aeb9920019f005713db55a8382e609f0c1
AntonVoronezh <pstut@mail.ru>
2019-04-23 13:48:55 +0300

    add items links

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,14 +1,31 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import classes from './Dialogs.module.css';
 
 const Dialogs = props => {
 	return (
 		<div className={classes.dialogs}>
 			<div className={classes.items}>
-				<div className={`${classes.items} ${classes.active}`}>11111111</div>
-				<div className={classes.items}>222222</div>
-				<div className={classes.items}>333333</div>
-				<div className={classes.items}>444444</div>
+				<div className={`${classes.items} ${classes.active}`}>
+					<NavLink to="/dialogs/1" activeClassName={classes.items}>
+						11111
+					</NavLink>
+				</div>
+				<div className={classes.items}>
+					<NavLink to="/dialogs/2" activeClassName={classes.items}>
+						22222
+					</NavLink>
+				</div>
+				<div className={classes.items}>
+					<NavLink to="/dialogs/3" activeClassName={classes.items}>
+						333333
+					</NavLink>
+				</div>
+				<div className={classes.items}>
+					<NavLink to="/dialogs/4" activeClassName={classes.items}>
+						4444444
+					</NavLink>
+				</div>
 			</div>
 			<div className={classes.messages}>
 				<div className={classes.message}>aaaaa</div>
