36378b183db5a698de52bd7d1ea0cf0503594b83
AntonVoronezh <pstut@mail.ru>
2019-04-24 17:21:48 +0300

    add state

--- src/index.js
+++ src/index.js
@@ -4,22 +4,6 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const messages = [
-	{ id: 1, text: '111 11111 11111 1111111' },
-	{ id: 2, text: '222 2222 22222 22222' },
-	{ id: 3, text: '33 33 333333 3333' },
-];
-
-const dialogs = [
-	{ id: 1, name: '1111111' },
-	{ id: 2, name: '2222222' },
-	{ id: 3, name: '333333' },
-	{ id: 4, name: '44444444' },
-	{ id: 5, name: '5555555' },
-	{ id: 6, name: '6666666' },
-];
-
-const posts = [{ id: 1, message: 'aaaaa aaaaaa', likeCount: 11 }, { id: 2, message: 'bbbbbb bbbbb', likeCount: 16 }];
 
 
 ReactDOM.render(<App messages={messages} dialogs={dialogs} posts={posts}/>, document.getElementById('root'));
