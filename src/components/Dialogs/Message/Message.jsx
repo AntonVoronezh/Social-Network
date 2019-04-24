5ae268de6717c2bf1264e55e9c6e0d079ae22ea2
AntonVoronezh <pstut@mail.ru>
2019-04-24 13:20:07 +0300

    refactoring

--- src/components/Dialogs/Message/Message.jsx
+++ src/components/Dialogs/Message/Message.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import classes from './Message.module.css';
 
-const Message = props => {
-	return <div className={classes.message}>{props.text}</div>;
-};
+const Message = props => <div className={classes.message}>{props.text}</div>;
 
 export default Message;
