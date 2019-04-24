6d60e11c94228a541d53d6f939cd8182d772e473
AntonVoronezh <pstut@mail.ru>
2019-04-24 10:14:27 +0300

    add postsData

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 import classes from './MyPosts.module.css';
 import Post from './Post/Post';
 
+const postsData = [
+	{ id: 1, message: 'aaaaa aaaaaa', likeCount: 11 },
+	{ id: 2, message: 'bbbbbb bbbbb', likeCount: 16 },
+
+];
+
 const MyPosts = () => {
 	return (
 		<div className={classes.content}>
@@ -15,7 +21,7 @@ const MyPosts = () => {
 				</div>
 			</div>
 			<div className={classes.posts}>
-				<Post message="aaaaa aaaaaa" likeCount="1" />
+				<Post message={postsData[0].message} likeCount={postsData[0].likeCount} />
 				<Post message="bbbbbb bbbbbbb" likeCount="4" />
 			</div>
 		</div>
