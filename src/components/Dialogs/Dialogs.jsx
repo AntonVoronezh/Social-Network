ebe3cc98ea978908721fd9e4ac0bef5332c64c86
AntonVoronezh <pstut@mail.ru>
2019-04-24 10:17:05 +0300

    dialogData

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,6 +2,16 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import classes from './Dialogs.module.css';
 
+const dialogData = [
+	{ id: 1, name: '1111111'},
+	{ id: 2, name: '2222222'},
+	{ id: 3, name: '333333'},
+	{ id: 4, name: '44444444'},
+	{ id: 5, name: '5555555'},
+	{ id: 6, name: '6666666'},
+
+];
+
 const Dialog = props => {
 	const path = `/dialogs/${props.id}`;
 
@@ -22,7 +32,7 @@ const Dialogs = props => {
 	return (
 		<div className={classes.dialogs}>
 			<div className={classes.items}>
-				<Dialog name="1111" id="1" />
+				<Dialog name={dialogData[0].name} id={dialogData[0].id} />
 				<Dialog name="2222" id="2" />
 				<Dialog name="3333" id="3" />
 				<Dialog name="4444" id="4" />
