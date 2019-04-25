60e9cb094f1fc9cf4a7b2c4050f9fedc7027f6e2
AntonVoronezh <pstut@mail.ru>
2019-04-25 09:05:03 +0300

    add createRef to Dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -9,10 +9,24 @@ const Dialogs = props => {
 	const dialogsData = dialogs.map(d => <Dialog name={d.name} id={d.id} key={d.id} />);
 	const messagesData = messages.map(m => <Message text={m.text} id={m.id} key={m.id} />);
 
+	const textareaData = React.createRef();
+
+	const buttonHandler = () => {
+		console.log('buttonHandler from Dialogs ', textareaData.current.value);
+	};
+
 	return (
 		<div className={classes.dialogs}>
 			<div className={classes.items}>{dialogsData}</div>
-			<div className={classes.messages}>{messagesData}</div>
+			<div className={classes.messages}>
+				{messagesData}
+				<div>
+					<textarea ref={textareaData} />
+				</div>
+				<div>
+					<button onClick={buttonHandler}>sub</button>
+				</div>
+			</div>
 		</div>
 	);
 };
