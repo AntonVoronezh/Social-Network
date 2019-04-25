10ce9c330c767c2b54d7b3b52f4a7838b0090cff
AntonVoronezh <pstut@mail.ru>
2019-04-25 08:56:45 +0300

    add

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -5,15 +5,21 @@ import Post from './Post/Post';
 const MyPosts = props => {
 	const postsData = props.posts.map(p => <Post message={p.message} id={p.id} key={p.id} likeCount={p.likeCount} />);
 
+	const textareaData = React.createRef();
+
+	const buttonHandler = () => {
+		console.log('buttonHandler from MyPosts ', textareaData.current.value);
+	};
+
 	return (
 		<div className={classes.content}>
 			<h3>New post</h3>
 			<div>
 				<div>
-					<textarea />
+					<textarea ref={textareaData} />
 				</div>
 				<div>
-					<button>sub</button>
+					<button onClick={buttonHandler}>sub</button>
 				</div>
 			</div>
 			<div className={classes.posts}>{postsData}</div>
