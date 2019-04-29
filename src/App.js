e54a507bc050c82387e2191b9efa3d397136bf08
AntonVoronezh <pstut@mail.ru>
2019-04-29 10:58:09 +0300

    refactoring App component

--- src/App.js
+++ src/App.js
@@ -10,16 +10,18 @@ import News from './components/News/News';
 import Settings from './components/Settings/Settings';
 
 const App = props => {
-	const { profilePage, dialogsPage } = props.state;
-	const { dispatch } = props;
+	// const { profilePage, dialogsPage } = props.state;
+	// const { dispatch } = props;
 
 	return (
 		<div className="wrapper">
 			<Header />
 			<Navbar />
 			<div className="content">
-				<Route path="/profile" render={() => <Profile state={profilePage} dispatch={dispatch} />} />
-				<Route path="/dialogs" render={() => <Dialogs state={dialogsPage} dispatch={dispatch} />} />
+				{/* <Route path="/profile" render={() => <Profile state={profilePage} dispatch={dispatch} />} />
+				<Route path="/dialogs" render={() => <Dialogs state={dialogsPage} dispatch={dispatch} />} /> */}
+				<Route path="/profile" component={Profile} />
+				<Route path="/dialogs" component={Dialogs} />
 				<Route path="/music" component={Music} />
 				<Route path="/news" component={News} />
 				<Route path="/settings" component={Settings} />
