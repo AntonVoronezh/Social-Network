a24be764fb04c189c5bd1306f4e19446a9e2f384
AntonVoronezh <pstut@mail.ru>
2019-04-30 15:38:46 +0300

    refactoring User

--- src/components/Users/User/User.jsx
+++ src/components/Users/User/User.jsx
@@ -1,12 +1,30 @@
 import React from 'react';
 import classes from './User.module.css';
+import Button from '../../UI/Button/Button';
 
 const User = props => {
-	return <div className={classes.user}>
-	
-	{props.status.id}
-	
-	</div>;
+	return (
+		<div className={classes.user}>
+			<div className={classes.followBlock}>
+				<img src={props.status.img} alt="dd" />
+				{props.followFlag ? (
+					<Button buttonHandler={props.unfollowCB} text="UnFollow" />
+				) : (
+					<Button buttonHandler={props.followCB} text="Follow" />
+				)}
+			</div>
+			<div className={classes.infoBlock}>
+				<div>
+					{props.status.firstName}
+					{props.status.status}
+				</div>
+				<div>
+					{props.status.location.country}
+					{props.status.location.sity}
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default User;
