9f504cc125719c5e18b7450c4181a9264e1b8971
AntonVoronezh <pstut@mail.ru>
2019-04-30 14:30:46 +0300

    refactoring Users container

--- src/containers/Users/Users.js
+++ src/containers/Users/Users.js
@@ -1,12 +1,19 @@
 import { connect } from 'react-redux';
-import Users from '../../components/Users/Users'
+import Users from '../../components/Users/Users';
+import { followAC, unfollowAC, setUsers } from '../../redux/reducers/usersReducer';
 
 const mapStateToProps = state => {
-	return {};
+	return {
+		state: state.usersPage,
+	};
 };
 
 const mapDispatchToProps = dispatch => {
-	return {};
+	return {
+		followCB: id => dispatch(followAC(id)),
+        unfollowCB: id => dispatch(unfollowAC(id)),
+        setUsersCB: users => dispatch(setUsers(users)),        
+	};
 };
 
 export default connect(
